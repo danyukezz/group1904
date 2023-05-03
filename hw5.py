fc31ea171573d8540c8ef748698553081797e130
Danya Bordunov <bordunovden2@gmail.com>
2023-05-03 21:14:48 +0200

    commit 'delete'

--- hw5.py
+++
@@ -1,4 +0,0 @@
-name = input('Please enter your name -').title()
-surname = input('Please enter your surname -').upper()
-message = f"Здарова, {name} {surname}, а ти знав, що твоє імя складається з {len(name)} літер?"
-print(message)
