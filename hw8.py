33dd92385a36bddde35d4f089ba7e866fe9b2215
Danya Bordunov <bordunovden2@gmail.com>
2023-05-09 13:04:30 +0200

    hw8.py

--- hw8.py
+++ hw8.py
@@ -1,30 +1,23 @@
 login = input('Enter your login >>> ')
-
-if login.isalpha():
-    pass
-else:
-    print('Only letters are acceptable!')
-
 password = input('Enter your password >>> ')
 
-if password.isalnum():
-    pass
+if not login.isalpha():
+    print('Login must contain only letters!')
+elif not password.isalnum():
+    print('Password must contain only letters or/and numbers!')
 else:
-    print('Incorrect data!')
-
-x = input('Confirm your password >>> ')
-if x == password:
-    print('You are logged in!')
-else:
-    print('Incorrect password!')
-
-print('You were logged out due to your teacher preferences')
-login_1 = input('Confirm your login again >>> ')
-password_1 = input('Confirm your password again >>> ')
-if login_1 == login and password_1 == password:
-    print('Congratulations! You are logged in again!')
-else:
-    print('Incorrect data,try one more time!')
+    x = input('Confirm your password >>> ')
+    if x == password:
+        print('You are logged in!')
+        print('You were logged out due to your teacher preferences')
+        login_1 = input('Confirm your login again >>> ')
+        password_1 = input('Confirm your password again >>> ')
+        if login_1 == login and password_1 == password:
+            print('Congratulations! You are logged in again!')
+        else:
+            print('Incorrect data,try one more time!')
+    else:
+        print('Incorrect password!')
 
 
 
