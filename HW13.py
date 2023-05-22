aecd1b1d2ded78ee9a1400a693027a280a297ead
Danya Bordunov <bordunovden2@gmail.com>
2023-05-22 16:09:30 +0200

    L-9

--- HW13.py
+++ HW13.py
@@ -10,7 +10,8 @@ total_cost = 0
 
 for price in inner_data['data']:
     price_products = price.get('price')
-    total_cost += price_products
+    quantity = price.get('rest')
+    total_cost += price_products * quantity
 pprint(total_cost)
 
 total_cost_without_gluten = 0
@@ -18,5 +19,6 @@ total_cost_without_gluten = 0
 for gluten in inner_data['data']:
     if gluten['gluten'] is False:
         price_products_without_gluten = gluten.get('price')
-        total_cost_without_gluten += price_products_without_gluten
+        quantity_without_gluten = gluten.get('rest')
+        total_cost_without_gluten += price_products_without_gluten * quantity_without_gluten
 pprint(total_cost_without_gluten)
--- hw14.py
+++ hw14.py
@@ -5,11 +5,12 @@ url = 'https://dummyjson.com/users'
 response = requests.get(url)
 inner_data = response.json()
 
-total_age = 0
+men_with_brown_hair = [user['age'] for user in inner_data['users'] if user['gender'] == 'male' and user['hair']['color'] == 'Brown']
+if men_with_brown_hair:
+    average_age_men_with_brown_hair = sum(men_with_brown_hair) / len(men_with_brown_hair)
+    pprint(f"Average age of men with brown hair: {average_age_men_with_brown_hair}")
 
-for hair_color in inner_data['users']:
-    if hair_color['color'] == 'Brown':
-        for user in inner_data['users']:
-            total_age += user['age']
-            avg_age_people_with_brown_hair = total_age / (len(inner_data['users']))
-        pprint(round(avg_age_people_with_brown_hair))
+louisville_citizen = [user for user in inner_data['users'] if user['address']['city'] == 'Louisville']
+if louisville_citizen:
+    for user in louisville_citizen:
+    pprint(user['firstName'], user['lastName'])
