5ae25f9b4d6183c2253e5e64a267f51094649b26
Danya Bordunov <bordunovden2@gmail.com>
2023-05-22 16:15:56 +0200

    L-9

--- hw14.py
+++ hw14.py
@@ -10,7 +10,7 @@ if men_with_brown_hair:
     average_age_men_with_brown_hair = sum(men_with_brown_hair) / len(men_with_brown_hair)
     pprint(f"Average age of men with brown hair: {average_age_men_with_brown_hair}")
 
-louisville_citizen = [user for user in inner_data['users'] if user['address']['city'] == 'Louisville']
+louisville_citizen = [user for user in inner_data['users'] if user['address'].get('city') == 'Louisville']
 if louisville_citizen:
-    for user in louisville_citizen:
-    pprint(user['firstName'], user['lastName'])
+    for user_1 in louisville_citizen:
+        print(user_1['firstName'], user_1['lastName'])
