f61bdae019c28c28219d800af8c55efb44fa27dc
Danya Bordunov <bordunovden2@gmail.com>
2023-05-23 14:48:26 +0200

    L-10

--- hw15.py
+++ hw15.py
@@ -8,17 +8,25 @@ def joke(value):
         return 'Анекдот про кота'
 print(joke(1))
 
-def perimeter():
+def perimeter() -> float:
     perimeter_rectangle = (width + height) * 2
     floated_perimeter_rectangle = float(perimeter_rectangle)
     return floated_perimeter_rectangle
-
 width = 10
 height = 20
 print(perimeter())
 
 
 def string(letter):
-    bad_letters = letter.replace('ї', '').replace('ж', '').replace('Ї', '').replace('Ж', '')
-    return bad_letters
+    bad_letters = ['Ї', 'ї', 'ж', 'Ж']
+    for letters in bad_letters:
+        letter = letter.replace(letters, '')
+    return letter
 print(string('хижак та їжак та їжа'))
+
+def remove_letters(letter, letters_1):
+    for twice_letter in letters_1:
+        letter = letter.replace(twice_letter.lower(), '')
+        letter = letter.replace(twice_letter.upper(), '')
+    return letter
+print(remove_letters("хижак", "вікно"))
